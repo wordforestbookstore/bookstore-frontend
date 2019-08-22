43176e7c68ab0be844a9dab5e36dd889023b5ff2
X1 Carbon <yjl9903@vip.qq.com>
2019-08-22 15:57:39 +0800

    init book store

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,83 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app dark>
+      <v-toolbar-title class="headline">
+        <router-link class="white--text" to="/">辞林书店</router-link>
+      </v-toolbar-title>
+      <v-menu offset-y open-on-hover class="ml-5">
+        <template v-slot:activator="{ on }">
+          <v-btn text dark v-on="on">菜单</v-btn>
+        </template>
+        <v-list>
+          <v-list-item key="add" to="/book/add">
+            <v-list-item-title>浏览书架</v-list-item-title>
+          </v-list-item>
+          <v-list-item key="list" to="/book/bookList">
+            <v-list-item-title>实体店信息</v-list-item-title>
+          </v-list-item>
+          <v-list-item key="faq" to="/book/bookList">
+            <v-list-item-title>FAQ</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+      
+      <v-row class="ml-1">
+        <v-col md="6">
+          <v-text-field v-model="search" 
+            light rounded solo clearable hide-details single-line>
+            <template v-slot:prepend-inner>
+              <v-icon>search</v-icon>
+            </template>
+          </v-text-field>
+        </v-col>
+      </v-row>
+
+      <v-spacer></v-spacer>
+      <v-btn text to="/login">
+        <span>登录</span>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+
+    <v-footer dark absolute>
+      <v-row>
+        <v-col class="text-left">&copy; 2017 - All Rights Reserved</v-col>
+        <v-col align-self="end" class="text-right">
+          <span>关于我们</span>
+          <span class="no-select mx-2">/</span>
+          <span>博客</span>
+          <span class="no-select mx-2">/</span>
+          <span>FAQ</span>
+          <span class="no-select mx-2">/</span>
+          <span>联系我们</span>
+          <span class="no-select mx-2">/</span>
+          <span>网站地图</span>
+        </v-col>
+      </v-row>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+
+  },
+  data: () => ({
+    search: '',
+  }),
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+a {
+  text-decoration: none;
+}
+.no-select {
+  user-select: none;
 }
 </style>
---
+++ src/components/index.vue
@@ -0,0 +1,44 @@
+<template>
+<v-container>
+  <v-row>
+    <v-col cols="9">
+      <v-carousel cycle height="275px">
+        <v-carousel-item
+          v-for="(item, i) in images"
+          :key="i">
+          <v-img :src="item"></v-img>
+        </v-carousel-item>
+      </v-carousel>
+    </v-col>
+    <v-col cols="3" align-self="center">
+      <v-img src="../assets/logo.png" max-height="275px"></v-img>
+    </v-col>
+  </v-row>
+  <v-row><v-divider></v-divider></v-row>
+
+</v-container>
+</template>
+
+<script>
+export default {
+  name: 'indexPage',
+  data: () => ({
+    images: [
+      require('../assets/sports-q-c-1600-500-1.jpg'),
+      require('../assets/sports-q-c-1600-500-2.jpg'),
+      require('../assets/sports-q-c-1600-500-3.jpg')
+    ],
+    colors: [
+      'primary',
+      'secondary',
+      'yellow darken-2',
+      'red',
+      'orange',
+    ],
+  }),
+}
+</script>
+
+<style>
+
+</style>
