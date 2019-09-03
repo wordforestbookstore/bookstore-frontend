d0ae71b82c181b9c06add164f30fc1c9c5f0bb8a
X1 Carbon <yjl9903@vip.qq.com>
2019-09-03 19:20:46 +0800

    add: head picture

---
+++ src/components/headpic.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="head-picture">
+    <v-row>
+      <v-col align-self="center" cols="9">
+        <h1 class="title">
+          <span color="black">{{ this.title }}</span>
+        </h1>
+        <div class="line"></div>
+      </v-col>
+      <v-col cols="3">
+        <v-img src="../assets/logo.png"></v-img>
+      </v-col>
+    </v-row>
+    <v-row class="mb-5">
+      <v-col>
+        <v-img max-height="8em" src="../assets/wood.png"></v-img>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'head-picture',
+  data: () => ({
+
+  }),
+  props: [
+    'title'
+  ]
+}
+</script>
+
+<style>
+.head-picture .title {
+  text-align: center;
+}
+.head-picture .title>span {
+  display: inline-block;
+  padding: 0.3em 1em;
+  color: white;
+  background-color: black;
+}
+.head-picture .line {
+  width: 100%;
+  height: 0.5em;
+  background-color: black;
+}
+</style>
