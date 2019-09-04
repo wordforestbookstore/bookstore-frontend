3e86911d86526acef6c3f1f6616f876487b11e7f
X1 Carbon <yjl9903@vip.qq.com>
2019-09-04 21:57:33 +0800

    add: meta title

--- src/router.js
+++ src/router.js
@@ -11,15 +11,24 @@ const router = new Router({
   routes: [
     {
       path: '/',
-      component: resolve => require(['./components/index.vue'], resolve)
+      component: resolve => require(['./components/index.vue'], resolve),
+      meta: {
+        title: '首页'
+      }
     },
     {
       path: '/hours',
-      component: resolve => require(['./components/info.vue'], resolve)
+      component: resolve => require(['./components/info.vue'], resolve),
+      meta: {
+        title: '实体店信息'
+      }
     },
     {
       path: '/faq',
-      component: resolve => require(['./components/faq.vue'], resolve)
+      component: resolve => require(['./components/faq.vue'], resolve),
+      meta: {
+        title: 'FAQ'
+      }
     },
     {
       path: '/user',
@@ -42,19 +51,63 @@ const router = new Router({
             },
             {
               path: 'edit',
-              component: resolve => require(['./userview/forms/useredit.vue'], resolve)
+              component: resolve => require(['./userview/forms/useredit.vue'], resolve),
+              meta: {
+                title: '编辑用户信息'
+              }
             },
             {
               path: 'orders',
-              component: resolve => require(['./userview/orders/index.vue'], resolve)
+              component: resolve => require(['./userview/orders/index.vue'], resolve),
+              meta: {
+                title: '用户订单'
+              }
             },
             {
               path: 'billings',
               component: resolve => require(['./userview/billings/index.vue'], resolve),
+              children: [
+                {
+                  path: '',
+                  component: resolve => require(['./userview/billings/list.vue'], resolve),
+                  meta: {
+                    title: '信用卡'
+                  }
+                },
+                {
+                  path: 'add',
+                  component: resolve => require(['./userview/billings/add.vue'], resolve),
+                  meta: {
+                    title: '添加信用卡'
+                  }
+                },
+                {
+                  path: 'update'
+                }
+              ]
             },
             {
               path: 'shipping',
-              component: resolve => require(['./userview/shipping/index.vue'], resolve)
+              component: resolve => require(['./userview/shipping/index.vue'], resolve),
+              children: [
+                {
+                  path: '',
+                  component: resolve => require(['./userview/shipping/list.vue'], resolve),
+                  meta: {
+                    title: '邮寄地址'
+                  }
+                },
+                {
+                  path: 'add',
+                  component: resolve => require(['./userview/shipping/add.vue'], resolve),
+                  meta: {
+                    title: '添加邮寄地址'
+                  }
+                },
+                {
+                  path: 'update'
+                }
+              ]
             }
           ]
         },
@@ -62,19 +115,25 @@ const router = new Router({
           path: 'login',
           component: resolve => require(['./userview/login.vue'], resolve)
         }
-      ]
+      ],
+      meta: {
+        title: '账户'
+      }
     },
     {
       path: '/newUser',
-      component: resolve => require(['./userview/newuser.vue'], resolve),
+      component: resolve => require(['./userview/newuser.vue'], resolve)
     },
     {
       path: '/book',
       component: resolve => require(['./bookview/book.vue'], resolve),
       children: [
         {
-          path: '/',
-          component: resolve => require(['./bookview/booklist.vue'], resolve)
+          path: '',
+          component: resolve => require(['./bookview/booklist.vue'], resolve),
+          meta: {
+            title: '书架'
+          }
         },
         {
           path: 'bookInfo',
@@ -98,6 +157,9 @@ const router = new Router({
         } else {
           next('/');
         }
+      },
+      meta: {
+        title: '购物车'
       }
     },
     {
@@ -110,9 +172,19 @@ const router = new Router({
         } else {
           next('/');
         }
+      },
+      meta: {
+        title: '创建订单'
       }
     }
   ]
+});
+
+router.beforeEach((to, from, next) => {
+  if (hasOwn(to, 'meta') && hasOwn(to.meta, 'title')) {
+    document.title = to.meta.title + ' | 辞林书店';
+  }
+  next();
 });
 
 export default router;
